48c1b84f94340c3c8d9815c87c86b2263d5432bc
Mona.R <manooshmona98@gmail.com>
2020-11-18 22:42:36 +0200

    Application

---
+++ MONA.R/Page6.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MONA.R
+{
+    public partial class Page6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            if (Application["Visit"] != null)
+            {
+                count = Convert.ToInt32(Application["Visit"].ToString());
+            }
+
+            count = count + 1;
+            Application["Visit"] = count;
+            Label1.Text = "Total Number Is = " + count.ToString();
+        }
+    }
+}
